9daa83fd6dd79493a9d809cee96544b4478ff74b
Josh McLaughlin <josh@opencraft.com>
2018-09-17 23:39:15 -0700

    Move announcements configuration to django plugin app configuration

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -270,10 +270,6 @@ FEATURES = {
     # Let students save and manage their annotations
     'ENABLE_EDXNOTES': False,
 
-    # Show site-level announcements on the course dashboard
-    'ENABLE_ANNOUNCEMENTS': False,
-    'ANNOUNCEMENTS_PER_PAGE': 5,
-
     # Milestones application flag
     'MILESTONES_APP': False,
 
@@ -2284,7 +2280,6 @@ INSTALLED_APPS = [
     'lms.djangoapps.course_goals',
 
     # Features
-    'openedx.features.announcements',
     'openedx.features.course_bookmarks',
     'openedx.features.course_experience',
     'openedx.features.course_search',
--- lms/envs/test.py
+++ lms/envs/test.py
@@ -78,9 +78,6 @@ FEATURES['ENABLE_ENROLLMENT_TRACK_USER_PARTITION'] = True
 
 FEATURES['ENABLE_BULK_ENROLLMENT_VIEW'] = True
 
-# Enable the platform announcements feature
-FEATURES['ENABLE_ANNOUNCEMENTS'] = True
-
 DEFAULT_MOBILE_AVAILABLE = True
 
 # Need wiki for courseware views to work. TODO (vshnayder): shouldn't need it.
